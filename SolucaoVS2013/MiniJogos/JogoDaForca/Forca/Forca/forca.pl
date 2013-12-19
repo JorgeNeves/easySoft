4deedcef94250b69b126b60054c12596d4c1110c
joao_1110297 <111297@isep.ipp.pt>
2013-12-19 15:24:28 +0000

    plengine

---
+++ SolucaoVS2013/MiniJogos/JogoDaForca/Forca/Forca/forca.pl
@@ -0,0 +1,18 @@
+%% Recebe uma string no formato compara("FORCA","A",L) e devolve o
+%% codigo ASCII da palavra encontrada ou seja L=[65].
+%% Outro exemplo: compara ("JOAO","O",L) retorna L=[79,79].
+
+compara(Palavra,Tentativa,L):-compara1(Palavra,Tentativa,L).
+compara1([],_,[]).
+compara1([H|T],[H],[H|L]):-!,compara1(T,[H],L).
+compara1([_|T],H,L):-!,compara1(T,H,L).
+
+%% Identico ao anterior mas retorna a posição em que as palavras se
+%% encontram.
+%% Exemplo:comparacont("JOAO","O",L) retorna L=[2,4].
+comparacont(Palavra,Tentativa,L):-compara2(Palavra,Tentativa,1,L).
+compara2([],_,_,[]).
+compara2([H|T],[H],I,[I|L]):-!,I1 is I+1,compara2(T,[H],I1,L).
+compara2([_|T],H,I,L):-!,I1 is I+1,compara2(T,H,I1,L).
+
+
