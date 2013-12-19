4ff25d7eee0e3b2c5f4aaeff20a3efcfd5009cb3
antoniopedro <antoniopfpeixoto@gmail.com>
2013-12-19 15:54:41 +0000

    Alteração do template usado no site. Login, Register e Pagina de Perfil já implementadas. Inicio da criação de página de edição de perfil.
    Issues #19 #16 #53 #52

---
+++ Site/WebApplication5/WebApplication5/Account/Login.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebApplication5.TabelModel.BLL;
+namespace WebApplication5.Account
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string user = TextBox1.Text;
+            string pass = TextBox2.Text;
+            int userID = Users.verifyuser(user, pass);
+            if (userID != -1)
+            {
+                //string type = Users.userType(user);
+                string type = "normal";
+                Session["userID"] = userID;
+                Session["username"] = user;
+                Session["userType"] = type;
+                if (type.Equals("normal")) { Response.Redirect("~/Profile/Profile.aspx"); }
+               
+                
+            }else { Label3.Visible = true; }
+
+        }
+        }
+    }
