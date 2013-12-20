f5d5090d60f7d7a7ce275d85e71e797bd4d799d9
Joao Camelo <1110285@isep.ipp.pt>
2013-12-20 14:35:51 +0000

    Atualização da lógica do labirinto antes aceitava posições do tipo "11" ou "156" e agora aceita apenas posições do tipo "(1,2)" ou seja do tipo (x,y)

--- ComponenteIA/labirinto.pl
+++ ComponenteIA/labirinto.pl
@@ -1,14 +1,23 @@
+liga((1,1),(1,2)).
+liga((1,2),(1,3)).
+liga((1,3),(1,4)).
+liga((1,4),(2,4)).
+liga((2,4),(3,4)).
+liga((3,4),(3,3)).
+liga((3,4),(3,5)).
+liga((3,6),(3,5)).
 
-encontra_caminho(O,D,L):-encontra_caminho(O,D,[O],L).
+encontra_caminho((Xi,Yi),(Xf,Yf),L):-
+	encontra_caminho((Xi,Yi), (Xf,Yf), [(Xi,Yi)],L).
 
-encontra_caminho(D,D,LA,L):-reverse(LA,L).
+encontra_caminho((Xf,Yf),(Xf,Yf),LA,L):-reverse(LA,L).
 
-encontra_caminho(O,D,LA,L):-liga(O,X),
-	not(member(X,LA)),
-	encontra_caminho(X,D,[X|LA],L).
+encontra_caminho((Xi,Yi),(Xf,Yf),LA,L):-(liga((Xi,Yi),(X,Y));liga((X,Y),(Xi,Yi))),
+	not(member((X,Y),LA)),
+	encontra_caminho((X,Y),(Xf,Yf),[(X,Y)|LA],L).
 
-cam_curto(O,D,P):-findall((Tam, L),
-		(encontra_caminho(O, D, L), length(L,Tam)),
+cam_curto((Xi,Yi),(Xf,Yf),P):-findall((Tam, L),
+		(encontra_caminho((Xi,Yi), (Xf,Yf), L), length(L,Tam)),
 		LR),
 	sort( LR, LROrd),
 	LROrd = [(_,P)|_].
