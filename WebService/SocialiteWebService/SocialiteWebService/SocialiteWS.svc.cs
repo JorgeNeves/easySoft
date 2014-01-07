7f49703d1d4cb93f0ba378f46707c95ee9c34872
Pedro Leal <pedrombleal@outlook.com>
2014-01-07 15:32:45 +0000

    Prototipo WebService final.

---
+++ WebService/SocialiteWebService/SocialiteWebService/SocialiteWS.svc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SocialiteWebService
+{
+    public class SocialiteWS : SocialiteOperations, WSMonitor
+    {
+        public String Login(string username, string pwhash)
+        {
+            string rightUser = "user";
+            string rightPw = "pw";
+            if(username == rightUser && pwhash == rightPw)
+            {
+                return Guid.NewGuid().ToString();
+            }
+            else
+            {
+                return "0";
+            }
+        }
+
+        public String CheckStatus()
+        {
+            return "OK - WS Running";
+        }
+    }
+}
