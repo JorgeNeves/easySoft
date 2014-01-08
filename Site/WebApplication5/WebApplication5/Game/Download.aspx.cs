eb979cdfcd556cca0f5815d3106b8bce93bc4c64
antoniopedro <apedrofcp@hotmail.com>
2014-01-08 02:39:14 +0000

    Página para download do jogo já criada. Falta colocar .exe quando disponível.
    Issue #20

---
+++ Site/WebApplication5/WebApplication5/Game/Download.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication5.Game
+{
+    public partial class Download : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Clear();
+            Response.ContentType = "application/octet-stream";
+            Response.AddHeader("Content-Disposition", "attachment; filename=ola.txt");
+            Response.WriteFile(Server.MapPath("ola.txt"));
+            Response.End();
+            Label2.Visible = true;
+        }
+    }
+}
