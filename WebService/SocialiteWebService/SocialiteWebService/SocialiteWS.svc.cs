32b8a488d03f7289e34605c2c83c724ad632f613
Pedro Leal <pedrombleal@outlook.com>
2014-01-09 15:57:23 +0000

    Criado endpoint global do estado do serviço

--- WebService/SocialiteWebService/SocialiteWebService/SocialiteWS.svc.cs
+++ WebService/SocialiteWebService/SocialiteWebService/SocialiteWS.svc.cs
@@ -20,7 +20,9 @@ namespace SocialiteWebService
          * Verifica se um utilizador se encontra na base de dados, caso contrário, a verificação devolve null 
          * e o login falha. Caso sucesso, verifica a password. Se sucesso, cria um access token,
          * e guarda o utilizador num dicionario de token|utilizador para controlo de acessos.
+         * o uri é /login?user=[username]&pw=[password]
          */
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, UriTemplate = "/login?user={username}&pw={password}")]
         public String Login(string username, string password)
         {
             Users user = db.Users.FirstOrDefault(u => u.Nick == username);
@@ -36,7 +38,9 @@ namespace SocialiteWebService
         /* Efetua logout de um utilizador via token. Caso token se encontre no dicionario de utilizadores
          * ligados, remove o utilizador da lista, bloqueando o seu acesso sem proceder a um novo login
          * a fim de obter um novo token.
+         * O URI é /logout/[token do utilizador]
          */
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, UriTemplate = "/logout/{token}")]
         public String Logout(string token)
         {
             if(LoggedUsers.ContainsKey(token))
@@ -47,14 +51,21 @@ namespace SocialiteWebService
             return "Logout failed";
         }
 
-        public String CheckStatus()
+        /* Devolve o estado do serviço em Json, contendo o estado do mesmo, utilizadores totais
+         * e numero de utilizadores ligados.
+         * Uri é /status
+         */
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, UriTemplate = "/status")]
+        public ServiceStatus Status()
         {
-            return "OK - WS Running";
-        }
-
-        public int NumLoggedUsers()
-        {
-            return LoggedUsers.Count;
+            ServiceStatus payload =
+                new ServiceStatus()
+                {
+                    Status = "OK",
+                    TotalUsers = db.Users.Count(),
+                    OnlineUsers = LoggedUsers.Count
+                };
+            return payload;
         }
 
     }
