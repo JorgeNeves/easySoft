0313d4e48a09dcdeb0f28e45a400fd704c4a1694
Pedro Leal <pedrombleal@outlook.com>
2014-01-13 23:39:23 +0000

    Cliente de login com funcionalidades de login e logout implementadas

--- LoginClient/LoginClient/LoginClient/LoginClientController.cs
+++ LoginClient/LoginClient/LoginClient/LoginClientController.cs
@@ -6,23 +6,67 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
 using System.Windows.Forms;
+using System.IO;
 
 namespace LoginClient
 {
     class LoginClientController
     {
+        private string token;
+        public bool isLogged { get; set; }
         public LoginClientController()
         {
-
+            token = null;
+            isLogged = false;
         }
 
-        public string Login(string username, string password)
+        public void Login(string username, string password)
         {
             WebClient wc = new WebClient();
             string Uri = "/login?user=" + username + "&pw=" + password;
             string result = wc.DownloadString(ConfigurationManager.AppSettings["WebServiceUrl"] + Uri);
-            result = result.Substring(1, result.Length - 2); // remover as aspas iniciais e finais
-            return result;
+            token = result.Substring(1, result.Length - 2); // remover as aspas iniciais e finais
+            if (token == "fail")
+            {
+                throw new Exception("The username or password were incorrect.");
+            }
+            isLogged = true;
+            try
+            {
+                SaveToken();
+            }
+            catch(Exception)
+            {
+                Logout();
+            }
+        }
+
+        public void Logout()
+        {
+            WebClient wc = new WebClient();
+            string Uri = "/logout/" + token;
+            string result = wc.DownloadString(ConfigurationManager.AppSettings["WebServiceUrl"] + Uri);
+            if (result == "\"fail\"")
+            {
+                throw new Exception("Could not logout.");
+            }
+            isLogged = false;
+        }
+
+        private void SaveToken()
+        {
+            // %AppData%\SocialiteData\token.cfg
+            string datadir = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\";
+            datadir += ConfigurationManager.AppSettings["DataDir"];
+            StreamWriter writer = new StreamWriter(datadir + "token.cfg");
+            writer.WriteLine(token);
+            writer.Close();
+        }
+
+        public void SafeClose()
+        {
+            if(isLogged)
+                Logout();
         }
     }
 }
--- LoginClient/LoginClient/LoginClient/LoginForm.cs
+++ LoginClient/LoginClient/LoginClient/LoginForm.cs
@@ -18,6 +18,7 @@ namespace LoginClient
     {
         private Button NormalModeButton { get; set; }
         private Button AdvModeButton { get; set; }
+        private Button LogoutButton { get; set; }
         private LoginClientController controller;
         public LoginForm()
         {
@@ -26,27 +27,39 @@ namespace LoginClient
 
             NormalModeButton    = new Button();
             AdvModeButton       = new Button();
+            LogoutButton        = new Button();
 
             username_label.Text     = strings.username_str;
             password_label.Text     = strings.password_str;
             Login.Text              = strings.login_str;
             NormalModeButton.Text   = strings.normal_str;
             AdvModeButton.Text      = strings.advanced_str;
+            LogoutButton.Text       = strings.logout_str;
+            LogoutButton.Click      += Logout_Click;
+            NormalModeButton.Click += NormalMode_Click;
+            AdvModeButton.Click += AdvMode_Click;
 
-            
 
+            NormalModeButton.Width = 200;
+            AdvModeButton.Width = 200;
+            LogoutButton.Width = 200;
+        }
+
+        private void ShowLoggedMenu()
+        {
+            mainTable.Controls.Clear();
+            mainTable.Controls.Add(LogoutButton);
+            mainTable.Controls.Add(NormalModeButton);
+            mainTable.Controls.Add(AdvModeButton);
         }
 
         private void Login_Click(object sender, EventArgs e)
         {
             string user = usernameBox.Text;
             string pass = passwordBox.Text;
-            string remote = null;
             try
             {
-                remote = controller.Login(user, pass);
-                if (remote == "failed")
-                    throw new Exception("The username or password were incorrect.");
+                controller.Login(user, pass);
             }
             catch(WebException ex)
             {
@@ -58,22 +71,45 @@ namespace LoginClient
                 MessageBox.Show(ex.Message, "Critical Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
                 return;
             }
-            StreamWriter writer = new StreamWriter("D:\\test.txt");
-            remote = (remote == null ? "null" : remote);
-            writer.WriteLine(user + " " + pass + "->" + remote);
-            writer.Close();
+            ShowLoggedMenu();
             return;
-            if (remote == "null")
+        }
+
+        private void Logout_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                controller.Logout();
+            }
+            catch (WebException ex)
+            {
+                MessageBox.Show("Failed to connect. " + ex.Message, "Critical Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
                 return;
-            tableLayoutPanel1.Controls.Clear();
-            tableLayoutPanel1.Controls.Add(NormalModeButton);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Critical Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                return;
+            }
         }
 
         private void NormalMode_Click(object sender, EventArgs e)
         {
             //Process.Start(ConfigurationManager.AppSettings["GameCorePath"]);
             MessageBox.Show("3D APP START HERE");
-            Application.Exit();
+           // Application.Exit();
+        }
+
+        private void AdvMode_Click(object sender, EventArgs e)
+        {
+            //Process.Start(ConfigurationManager.AppSettings["GameCorePath"]);
+            MessageBox.Show("3D ADV APP START HERE");
+            // Application.Exit();
+        }
+
+        private void LoginForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            controller.SafeClose();
         }
     }
 }
