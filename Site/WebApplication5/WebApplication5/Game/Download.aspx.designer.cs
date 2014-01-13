4dd508d4a60abc6bf29bcc42d13982f9d1faba33
joao_1110297 <111297@isep.ipp.pt>
2014-01-13 17:40:22 +0000

    1110297 #20 Registo do Download na Base de Dados com a hora

--- Site/WebApplication5/WebApplication5/Game/Download.aspx.designer.cs
+++ Site/WebApplication5/WebApplication5/Game/Download.aspx.designer.cs
@@ -31,6 +31,15 @@ namespace WebApplication5.Game {
         protected global::System.Web.UI.WebControls.Button Button1;
         
         /// <summary>
+        /// Label3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+        
+        /// <summary>
         /// Label2 control.
         /// </summary>
         /// <remarks>
