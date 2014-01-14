b3ee2a1d29d072ba42d7129597a42849e8a3abad
joao_1110297 <111297@isep.ipp.pt>
2014-01-14 11:20:05 +0000

    1110297: close #48 versão final do script para monitorizar os downloads

--- Nagios/check_downloads.sh
+++ Nagios/check_downloads.sh
@@ -5,19 +5,18 @@
 #Downloads>5 Warning
 #Downloads>100 Critical
 #URL do WS
-#URL=http://wvm054.dei.isep.ipp.pt/SocialLiteWS/SocialiteWS.svc/status
-#RESPONSE=$(curl -i -H "Accept: application/json" -H "Content-Type: application/json" $URL | tail -1)
+URL=http://wvm054.dei.isep.ipp.pt/SocialLiteWS/SocialiteWS.svc/downloads
+RESPONSE=$(curl -i -H "Accept: application/json" -H "Content-Type: text/plain" $URL | tail -1| tr '"' ' ' )
 
-if [ $RESPONSE > 100 ]; then
-        echo "O numero de utilizadores e superior a 100";
-		exit 2;
+if [ $RESPONSE -gt 100 ]; then
+                echo "O numero de downloads superior a 100- Numero de Downloads esta hora:$RESPONSE";
+                exit 2;
 else
-        if [ $RESPONSE>5 ] then
-			echo "O numero de utilizadores e superior a 5";
-			exit 1;
-		fi
+        if [ $RESPONSE -gt 5 ]; then
+                echo "O numero de downloads e superior a 5-Numero de Downloads esta hora:$RESPONSE";
+                exit 1;
+        fi
 fi
-echo "Numero de Downloads igual ou inferior a 5";
+echo "Numero de Downloads igual ou inferior a 5-Numero de Downloads esta hora:$RESPONSE";
 exit 0;
-
 
