2bbe35d7796e3f4e721db80c6e9c9c60357a8725
Pedro Leal <pedrombleal@outlook.com>
2014-01-14 21:13:07 +0000

    Implementado modulo de inicialização do grafo. Obtem os dados do utilizador atual, e os IDs dos seus amigos e gera um grafo. É necessário ir buscar amigos dos amigos e dados de tais.

--- SolucaoVS2013/GameCore/GameCore/AdvancedModeLoader.h
+++ SolucaoVS2013/GameCore/GameCore/AdvancedModeLoader.h
@@ -44,7 +44,6 @@ public:
 
 	Node* GetUserFromId(int id)
 	{
-		Node* node = nullptr;
 		for (std::list<Node*>::iterator it = nodelist.begin(); it != nodelist.end(); it++)
 		{
 			if ((*it)->UserID == id)
@@ -57,16 +56,25 @@ public:
 class AdvancedGraphGenerator
 {
 	std::map <std::wstring, FieldValue> fieldMap;
-	Graph graph;
+	Graph* graph;
 
 public:
-	AdvancedGraphGenerator()
+	wstring url;
+
+	AdvancedGraphGenerator(wstring url)
 	{
 		fieldMap[L"UserFriendsIDs"] = FieldValue::UserFriendsIDs;
 		fieldMap[L"UserID"] = FieldValue::UserID;
 		fieldMap[L"UserMood"] = FieldValue::UserMood;
 		fieldMap[L"UserName"] = FieldValue::UserName;
 		fieldMap[L"Usertags"] = FieldValue::UserTags;
+		graph = new Graph();
+		this->url = url;
+	}
+
+	Graph* GetGeneratedGraph()
+	{
+		return graph;
 	}
 
 	void SetField(std::wstring name, json::value value, Node* node)
@@ -78,10 +86,16 @@ public:
 			{
 				Node* userfriend = new Node();
 				int id = iterInner->second.as_integer();
-				if (graph.GetUserFromId(id) == nullptr)
+				if (graph->GetUserFromId(id) == nullptr)
 				{
 					userfriend->UserID = id;
 					node->friends.push_back(userfriend);
+					graph->nodelist.push_back(userfriend);
+
+					Arch* arch = new Arch();
+					arch->friend1 = node->UserID;
+					arch->friend2 = userfriend->UserID;
+					graph->archlist.push_back(arch);
 				}
 			}
 			break;
@@ -104,7 +118,7 @@ public:
 		}
 	}
 
-	Graph GetGraphFromJson(json::value value)
+	Graph* GetGraphFromJson(json::value value)
 	{
 		Node* node = new Node();
 		for (auto iterInner = value.cbegin(); iterInner != value.cend(); ++iterInner)
@@ -114,37 +128,34 @@ public:
 
 			SetField(propertyName.as_string(), propertyValue, node);
 		}
-		graph.nodelist.push_back(node);
+		graph->nodelist.push_back(node);
 		return graph;
+	}
+
+	pplx::task<void> GetRemoteUserData()
+	{
+		return pplx::create_task([this]
+		{
+			http_client client(this->url);
+
+			return client.request(methods::GET);
+		}).then([](http_response response)
+		{
+			if (response.status_code() == status_codes::OK)
+			{
+				return response.extract_json();
+			}
+
+			return pplx::create_task([] { return json::value(); });
+
+		}).then([this](json::value jsonValue)
+		{
+			if (jsonValue.is_null())
+				return;
+
+			this->GetGraphFromJson(jsonValue);
+		});
 	}
 };
 
-pplx::task<void> GetAll()
-{
-	return pplx::create_task([]
-	{
-		http_client client(L"http://wvm054.dei.isep.ipp.pt/SocialLiteWS/SocialiteWS.svc/userdata?id=1&token=debug");
-
-		return client.request(methods::GET);
-	}).then([](http_response response)
-	{
-		if (response.status_code() == status_codes::OK)
-		{
-			return response.extract_json();
-		}
-
-		return pplx::create_task([] { return json::value(); });
-
-	}).then([](json::value jsonValue)
-	{
-		if (jsonValue.is_null())
-			return;
-
-		AdvancedGraphGenerator generator;
-		auto graph = generator.GetGraphFromJson(jsonValue);
-		/* graph build routines
-		StatusGenerator generator;
-		auto member = generator.GetStatusFromJson(jsonValue);
-		member.Display();*/
-	});
-}
+#endif
